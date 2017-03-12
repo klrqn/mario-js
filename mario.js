664374dac92c25f5c8704f57ede1777c034d7a57
john doe <scottbruceburton@gmail.com>
2017-03-12 16:13:17 -0400

    did pyramid with .repeat instead

--- mario.js
+++ mario.js
@@ -1,14 +1,21 @@
 
-/* this is a comment */
+printPyramid(6);
 
- function printPyramid(height) {
-     space = "";
-     while (space.length < height) {
-         space += " ";
-     }
 
-     for (var brick = "##"; brick.length < height+2; brick += "#") {
-         console.log(space + brick);
-         space = space.slice(0,-1);
-     }
- }
+function printPyramid(height) {
+    for (var i = 0; i < height; i++) {
+        console.log(" ".repeat(height-i-1) + "#".repeat(i+2));
+    }
+}
+// My First Try:
+
+//     space = "";
+//     while (space.length < height) {
+//         space += " ";
+//     }
+//
+//     for (var brick = "##"; brick.length < height+2; brick += "#") {
+//         console.log(space + brick);
+//         space = space.slice(0,-1);
+//     }
+// }
